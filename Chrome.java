01e01b0be10b07f632d5aedaa0b0fb4071b7e54f
Minumol888 <minumolmut@gmail.com>
2025-09-13 20:09:12 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package selenium;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+	
+	float version=91.0f;
+	String name= "Google";
+	boolean Betaoftype=false;
+	short releaseofYear=2008;
+	char shortcutkey='c';
+	System.out.println(name +" " + Betaoftype + " " + releaseofYear + " " + 'c');
+
+	}
+
+}
